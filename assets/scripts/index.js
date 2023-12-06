6504df877d3a9b3de4b99d2660d8c3a5a91e4bb9
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2023-12-06 16:18:54 +0100

    Pre Loading

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -38,7 +38,7 @@ const loadList = () => {
        
         if (obj === undefined || "" ){
             modalBox.innerHTML = `
-            <i class="fa-solid fa-ban"></i>
+            <i class="fa-solid fa-ban" style="color:red;"></i>
             <h3>Sorry!!! No admission record found.<span id="name"></span></h3>
             <div class="buttons">
               <button class="close-btn" onclick="hello()">Ok, Close</button>
@@ -53,7 +53,7 @@ const loadList = () => {
       modalBox.innerHTML = `
       <i class="fa-regular fa-circle-check"></i>
       <h3>Congratulations!!! <span id="name">${name}</span></h3>
-      <h3>You have been offered provisional Admission into the University OF DELTA, AGBOR, DELTA STATE.</h3>
+      <h3>You have been offered provisional Admission into the UNIVERSITY OF DELTA, AGBOR, DELTA STATE.</h3>
       <h3>Course:<span id="course">${course} </span></h3>
       <h3>Faculty: <span id="status">${status}</span></h3>
       <div class="buttons">
